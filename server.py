c7ee3f4d6477a5703a313ac49219bc9007170012
kieutra <kieutra@gmail.com>
2024-11-26 06:01:29 -0500

    task 8

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
-from flask import Flask, request, jsonify
+"""this module is a server"""
+from flask import Flask, request
 from EmotionDetection import emotion_detector
 app = Flask(__name__)
-
+"""POST method"""
 @app.route("/", methods = ['POST'])
-def emotionDetection():
-    text_to_analyze = request.get_data(as_text=True)       
-    result = emotion_detector(text_to_analyze);
-    if result["dominant_emotion"] == None:
+def get_emotion():
+    """get emotion by calling emotion_detector()"""
+    text_to_analyze = request.get_data(as_text=True)
+    result = emotion_detector(text_to_analyze)
+    if result["dominant_emotion"] is None:
         return "Invalid text!Please try again"
     return result
 
